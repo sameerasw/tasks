4412c505bd608b298c7c7faaa269e25ad9b34c59
sameerasw <sameerasw.com@gmail.com>
2025-10-10 21:16:36 +0530

    initial commit

---
+++ tasks/tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Sameera Sandakelum on 2025-10-10.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
